78ff90d211b1e938e60044436942ab28bd545cd3
DefectingCat <i@defect.ink>
2023-10-17 21:12:23 +0800

    Update args long about
    remove useless code

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use std::path::PathBuf;
 
 /// Cymo: Multi-threaded FTP Upload Tool
 ///
@@ -21,7 +20,21 @@ use std::path::PathBuf;
 /// println!("Local Path: {:?}", args.local_path);
 /// ```
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "Cymo is a command-line tool for multi-threaded FTP file uploads. It allows you to efficiently upload files and directories to an FTP server using multiple threads, improving upload speed and performance.
+
+You can specify the remote path on the FTP server, the local path to the files or directories to upload, and optional authentication credentials. Cymo simplifies the process of uploading large amounts of data to an FTP server with ease.
+
+Example Usage:
+To upload files to an FTP server, use a command like:
+$ cymo -r /ftp/upload -l /local/files -s ftp.example.com
+
+Or use username and password for authentication:
+$ cymo -r /ftp/upload -l /local/files -s ftp.example.com -u <username> -p <password>"
+)]
 pub struct Args {
     /// The remote path on the FTP server where files will be uploaded.
     #[arg(short, long)]
@@ -31,12 +44,15 @@ pub struct Args {
     #[arg(short, long)]
     pub local_path: String,
 
+    /// The FTP server address or hostname where the files will be uploaded.
     #[arg(short, long)]
     pub server: String,
 
+    /// The username for authenticating with the FTP server (optional).
     #[arg(short, long)]
     pub username: Option<String>,
 
+    /// The password for authenticating with the FTP server (optional).
     #[arg(short, long)]
     pub password: Option<String>,
 }
