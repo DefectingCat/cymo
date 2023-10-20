95bcc09accd9ad1030c12e517a5f25b2415443ed
L LJ <defect.y@outlook.com>
2023-10-20 00:56:00 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,18 @@
+/**
+* JetBrains Space Automation
+* This Kotlin script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+job("Build, run tests, and publish") {
+    container(displayName = "Run script", image = "rustlang/rust:nightly") {
+        shellScript {
+            content = """
+                set -e
+                # Run tests
+                cargo test --verbose --release
+                # Build the Rust project
+                cargo build --verbose --release
+            """
+        }
+    }
+}
