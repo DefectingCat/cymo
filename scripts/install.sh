2b61ae920f563c9e002beb53eb748f4b72a92018
DefectingCat <i@defect.ink>
2023-10-31 16:24:08 +0800

    add detect apple darwin platform

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-echo "hello"
+OS=""
+case "$OSTYPE" in
+    solaris*) echo "SOLARIS" ;;
+    darwin*)  OS="apple-darwin" ;;
+    linux*)   echo "LINUX" ;;
+    bsd*)     echo "BSD" ;;
+    msys*)    echo "WINDOWS" ;;
+    cygwin*)  echo "ALSO WINDOWS" ;;
+    *)        echo "unknown: $OSTYPE" ;;
+esac
+
+arch=""
+case "$(uname -m)" in
+    arm64*) arch="aarch64-" ;;
+    *) echo "unknown arch: $(uname -m)" ;;
+esac
+
+echo "$arch$OS"
