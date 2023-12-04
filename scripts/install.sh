3ae1d91eac6827b275a6101663d7b6db22eae416
DefectingCat <i@defect.ink>
2023-12-04 15:23:54 +0800

    add executable for install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,19 +2,19 @@
 
 OS=""
 case "$OSTYPE" in
-    solaris*) echo "SOLARIS" ;;
-    darwin*)  OS="apple-darwin" ;;
-    linux*)   echo "LINUX" ;;
-    bsd*)     echo "BSD" ;;
-    msys*)    echo "WINDOWS" ;;
-    cygwin*)  echo "ALSO WINDOWS" ;;
-    *)        echo "unknown: $OSTYPE" ;;
+solaris*) echo "SOLARIS" ;;
+darwin*) OS="apple-darwin" ;;
+linux*) echo "LINUX" ;;
+bsd*) echo "BSD" ;;
+msys*) echo "WINDOWS" ;;
+cygwin*) echo "ALSO WINDOWS" ;;
+*) echo "unknown: $OSTYPE" ;;
 esac
 
 arch=""
 case "$(uname -m)" in
-    arm64*) arch="aarch64-" ;;
-    *) echo "unknown arch: $(uname -m)" ;;
+arm64*) arch="aarch64-" ;;
+*) echo "unknown arch: $(uname -m)" ;;
 esac
 
 echo "$arch$OS"
@@ -28,5 +28,8 @@ function detectCurl() {
 
 detectCurl
 
+echo "Starting download"
 url="http://192.168.1.22/lilinjun/cymo/uploads/2a759444eeeeb1e7e26f6326aaa6db5b/cymo-$arch$OS"
 curl "$url" -o "cymo"
+chmod +x cymo
+echo "Install sucess"
